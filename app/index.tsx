d1e00499c851297b26ccc6483172e9b7eee4a987
anshry <faqih@anshry.dev>
2025-11-13 13:28:18 +0700

    finish input, scroll and list lesson

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,88 @@
-import { StyleSheet, View } from "react-native";
+import { useState } from "react";
+import { FlatList, StyleSheet, Text, TextInput, View } from "react-native";
 
 import { ShoppingListItem } from "../components/ShoppingListItem";
 import { theme } from "../theme";
 
+type ShoppingListItemType = {
+  id: string;
+  name: string;
+  isCompleted?: boolean;
+};
+
+const initialList: ShoppingListItemType[] = [
+  { id: "1", name: "Coffee" },
+  { id: "2", name: "Tea", isCompleted: true },
+  { id: "3", name: "Sugar", isCompleted: true },
+];
+
 export default function App() {
+  const [shoppingList, setShoppingList] = useState<ShoppingListItemType[]>([]);
+  const [value, setValue] = useState("");
+
+  const handleSubmit = () => {
+    if (value) {
+      const newShoppingList = [
+        {
+          id: new Date().toTimeString(),
+          name: value,
+        },
+        ...shoppingList,
+      ];
+      setShoppingList(newShoppingList);
+      setValue("");
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <ShoppingListItem name="Coffee" />
-      <ShoppingListItem name="Tea" isCompleted />
-      <ShoppingListItem name="Sugar" isCompleted />
-    </View>
+    <FlatList
+      data={shoppingList}
+      renderItem={({ item }) => (
+        <ShoppingListItem name={item.name} isCompleted={item.isCompleted} />
+      )}
+      style={styles.container}
+      contentContainerStyle={styles.contentContainer}
+      stickyHeaderIndices={[0]}
+      ListEmptyComponent={
+        <View style={styles.emptyListContainer}>
+          <Text>Your shopping list is empty!</Text>
+        </View>
+      }
+      ListHeaderComponent={
+        <TextInput
+          placeholder="E.g. Coffee"
+          style={styles.textInput}
+          value={value}
+          onChangeText={setValue}
+          returnKeyType="done"
+          onSubmitEditing={() => handleSubmit()}
+        />
+      }
+    />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 12,
     backgroundColor: theme.colors.white,
+  },
+  contentContainer: {
+    paddingBottom: 24,
+  },
+  textInput: {
+    borderColor: theme.colors.lightGrey,
+    borderWidth: 2,
+    padding: 12,
+    marginHorizontal: 12,
+    marginBottom: 12,
+    borderRadius: 50,
+    fontSize: 18,
+    backgroundColor: theme.colors.white,
+  },
+  emptyListContainer: {
+    alignItems: "center",
     justifyContent: "center",
   },
 });
